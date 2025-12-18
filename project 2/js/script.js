04e80d969e5f90f3af8a2758d5dd37b62350a683
Adbrskk <aedobroskok@gmail.com>
2025-12-18 10:29:49 +0100

    new project

--- project 2/js/script.js
+++ project 2/js/script.js
@@ -1,8 +1,22 @@
+class Order {
+    
+}
+class Notification {
+    constructor(title, content, date, icon = '', type = '', read = false) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.icon = icon;
+        this.type = type;
+        this.read = read;
+    }
+}
+
 const notification = {
     title: '',
     content: '',
     date: "",
-    icon: '🛒',
-    type: 'order',
+    icon: '',
+    type: '',
     read: false,
 }
