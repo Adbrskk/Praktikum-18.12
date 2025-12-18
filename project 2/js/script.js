88e6e94d6b831d5852d17820a5a04bd8562e8fa9
Adbrskk <aedobroskok@gmail.com>
2025-12-18 12:27:43 +0100

    html css

--- project 2/js/script.js
+++ project 2/js/script.js
@@ -1,22 +1,69 @@
-class Order {
-    
-}
 class Notification {
-    constructor(title, content, date, icon = '', type = '', read = false) {
+    constructor(title, content, type = 'success') {
         this.title = title;
         this.content = content;
-        this.date = date;
-        this.icon = icon;
         this.type = type;
-        this.read = read;
+        this.date = new Date().toLocaleTimeString();
+    }
+
+    render() {
+        const div = document.createElement('div');
+        div.className = `notification ${this.type}`;
+        div.innerHTML = `<strong>${this.title}</strong><br>${this.content}`;
+        
+        document.getElementById('notifications').append(div);
+
+        setTimeout(() => div.remove(), 3000);
     }
 }
 
-const notification = {
-    title: '',
-    content: '',
-    date: "",
-    icon: '',
-    type: '',
-    read: false,
+class Order {
+    create() {
+        new Notification(
+            'Заказ создан',
+            'Ожидайте дальнейшей информации',
+            'success'
+        ).render();
+    }
+
+    paid() {
+        new Notification(
+            'Заказ оплачен',
+            'Ожидайте отправки',
+            'warning'
+        ).render();
+    }
+
+    sent() {
+        new Notification(
+            'Заказ отправлен',
+            'Ожидайте курьера',
+            'warning'
+        ).render();
+    }
+
+    received() {
+        new Notification(
+            'Заказ получен',
+            'Ждём вас снова!',
+            'success'
+        ).render();
+    }
 }
+
+
+const form = document.getElementById('orderForm');
+const actions = document.getElementById('orderActions');
+const order = new Order();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    order.create();
+    actions.classList.remove('hidden');
+});
+
+actions.addEventListener('click', (e) => {
+    if (e.target.dataset.status === 'paid') order.paid();
+    if (e.target.dataset.status === 'sent') order.sent();
+    if (e.target.dataset.status === 'received') order.received();
+});
